65ac9d30d42f0d7898c459146e96521a03cb37fe
idriverag4 <idriverag7@gmail.com>
2022-10-05 14:33:18 -0500

    Se crea el helper de los datos de la seccion 1

---
+++ src/helpers/DataSection1.jsx
@@ -0,0 +1,24 @@
+import { faHandshake, faMoneyCheckDollar, faFillDrip, faListCheck } from '@fortawesome/free-solid-svg-icons'
+
+export const DataSection1 = [
+    {
+        icon: faHandshake,
+        title: <h3>Calidad y Confianza</h3>,
+        desc: <p>Contamos con más de <span className="text-primary" style={{ fontWeight: '900' }}>7 años</span> de experiencia. Esto nos hace tu mejor opción.</p>,
+    },
+    {
+        icon: faMoneyCheckDollar,
+        title: <h3>Precios Competitivos</h3>,
+        desc: <p>Nuestro principal objetivo es brindarte la mejor alternativa económica.</p>,
+    },
+    {
+        icon: faFillDrip,
+        title: <h3>Productos de Calidad</h3>,
+        desc: <p>Utilizamos los mejores productos del mercado para brindar excelentes resultados.</p>,
+    },
+    {
+        icon: faListCheck,
+        title: <h3>Garantía del Servicio</h3>,
+        desc: <p>Ofrecemos total garantía de calidad en cada uno de nuestros servicios.</p>,
+    },
+]
